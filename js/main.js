e0d357d0543be60c95d0006de4a37e6b2ab89f17
dearnono2 <dearnono2@gmail.com>
2022-10-11 01:37:20 +0900

    main slide 미완

---
+++ js/main.js
@@ -0,0 +1,129 @@
+/* section - occasion 영역 */
+
+const wrap = document.querySelector('.wrap');
+const wrapImgs = wrap.querySelector('.wrap-imgs');
+const articles = wrap.querySelectorAll('article');
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+// 오류문제
+// 1.슬라이드가 부드럽게 안됨.
+// 2.초기 위치값 지정이 안먹힘.
+
+let len = articles.length;
+
+let enableClick = true;
+
+init();
+
+prev.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if(enableClick) {
+    enableClick = false;
+
+    prevSlide();
+  }
+})
+
+next.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if(enableClick) {
+    enableClick = false;
+
+    nextSlide();
+  }
+})
+
+// 초기화 함수
+function init() {
+  wrapImgs.style.left = '-290px';
+
+  wrapImgs.prepend(wrapImgs.lastElementChild);
+
+  wrapImgs.style.width = `${290 * len}px`;
+  articles.forEach((el) => {
+    el.style.width = `${1500 / len}px`;
+  })
+}
+
+function prevSlide() {
+  new Anim(wrapImgs, {
+    prop : 'left',
+    value: '0',
+    duration : 1000,
+    callback : () => {
+      wrapImgs.style.left = '-290px';
+      wrapImgs.prepend(wrapImgs.lastElementChild);
+      enableClick = true;
+    }
+  })
+}
+
+function nextSlide() {
+  new Anim(wrapImgs, {
+    prop : 'left',
+    value: '-580px',
+    duration : 1000,
+    callback : () => {
+      wrapImgs.style.left = '-290px';
+      wrapImgs.append(wrapImgs.firstElementChild);
+      enableClick = true;
+    }
+  })
+}
+
+/* // section - occasion 영역 */
+
+
+/* section - why choose seoul flower? 영역 */
+const whySeoulFlower = document.querySelector('#why-seoul-flower');
+const wrapReasons = whySeoulFlower.querySelector('.wrap');
+const reasons = whySeoulFlower.querySelectorAll('.reason');
+
+
+
+window.addEventListener('scroll', (e) => {
+  console.log(window.scrollY);
+  if(window.scrollY > 3400) {
+    reasons[0].classList.add('show');
+  } else if (window.scrollY > 4000) {
+    reasons[0].classList.remove('show');
+  }
+
+  if(window.scrollY > 4200) {
+    reasons[1].classList.add('show');
+  } else if (window.scrollY > 4800) {
+    reasons[1].classList.remove('show');
+  }
+
+  if(window.scrollY > 5000) {
+    reasons[2].classList.add('show');
+  } else if (window.scrollY > 5600) {
+    reasons[2].classList.remove('show');
+  }
+})
+
+/* // section - why choose seoul flower? 영역 */
+
+
+// wrapReasons.addEventListener('scroll', checkReasons)
+
+// checkReasons();
+
+// function checkReasons() {
+//   // const triggerBottom = window.innerHeight / 5 * 4;
+//   console.log(window.innerHeight / 5 * 4);
+
+//   reasons.forEach(reason => {
+//     const  reasonTop = reason.getBoundingClientRect().top;
+
+//     if(reasonTop < triggerBottom) {
+//       reason.classList.add('show');
+//     } else {
+//       reason.classList.remove('show');
+//     }
+//   })
+// }
