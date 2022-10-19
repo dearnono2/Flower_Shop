648063086fa3ac31e2d919ee359d501bdb5e0d48
dearnono2 <dearnono2@gmail.com>
2022-10-19 17:44:46 +0900

    shop 부분 진행중

---
+++ js/shop.js
@@ -0,0 +1,39 @@
+let productMiddle = document.querySelector('.products-middle');
+
+let products = `                
+<div class="product-items">
+  <div class="item">
+    <div class="img">
+      <img src="https://via.placeholder.com/300" alt="">
+    </div>
+    <div class="txt">
+      <p><b>PRODUCT</b></p>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  </div>
+  <div class="item">
+    <div class="img">
+      <img src="https://via.placeholder.com/300" alt="">
+    </div>
+    <div class="txt">
+      <p><b>PRODUCT</b></p>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  </div>
+  <div class="item">
+    <div class="img">
+      <img src="https://via.placeholder.com/300" alt="">
+    </div>
+    <div class="txt">
+      <p><b>PRODUCT</b></p>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  </div>
+</div>`;
+
+
+for(let i = 1; i <= 3; i++) {
+  productMiddle.insertAdjacentHTML('beforeend', products);
+}
+
+
